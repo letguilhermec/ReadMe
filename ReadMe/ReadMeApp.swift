fc6712535401302e4e63cc8ee6dbf5788ab97889
Guilherme Correa <correacgui@gmail.com>
2023-04-24 17:08:36 -0300

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Guilherme de Carvalho Correa on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
